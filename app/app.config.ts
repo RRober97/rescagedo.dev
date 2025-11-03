1639435cd50353a43b14609ffbe84e4f440df9f7
Roberto <robertoea97@gmail.com>
2025-11-03 17:33:46 +0100

    init: primera versión limpia

---
+++ app/app.config.ts
@@ -0,0 +1,40 @@
+export default defineAppConfig({
+  global: {
+    picture: {
+      dark: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      light: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      alt: 'Mi foto de perfil'
+    },
+    meetingLink: 'https://meet.google.com/landing',
+    email: 'robertoea97@gmail.com',
+    available: true
+  },
+  ui: {
+    colors: {
+      primary: 'blue',
+      neutral: 'neutral'
+    },
+    pageHero: {
+      slots: {
+        container: 'py-18 sm:py-24 lg:py-32',
+        title: 'mx-auto max-w-xl text-pretty text-3xl sm:text-4xl lg:text-5xl',
+        description: 'mt-2 text-md mx-auto max-w-2xl text-pretty sm:text-md text-muted'
+      }
+    }
+  },
+  footer: {
+    credits: `Roberto Escagedo Alonsotegui • © ${new Date().getFullYear()}`,
+    colorMode: false,
+    links: [{
+      'icon': 'i-simple-icons-github',
+      'to': 'https://github.com/RRober97',
+      'target': '_blank',
+      'aria-label': 'Mi perfil de GitHub'
+    }, {
+      'icon': 'i-simple-icons-linkedin',
+      'to': 'https://www.linkedin.com/in/roberto-escagedo-alonsotegui/',
+      'target': '_blank',
+      'aria-label': 'Mi perfil de Linkedin'
+    }]
+  }
+})
---
+++ app/utils/links.ts
@@ -0,0 +1,15 @@
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+export const navLinks: NavigationMenuItem[] = [{
+  label: 'Inicio',
+  icon: 'i-lucide-home',
+  to: '/'
+}, {
+  label: 'Proyectos',
+  icon: 'i-lucide-folder',
+  to: '/projects'
+}, {
+  label: 'Acerca de mí',
+  icon: 'i-lucide-user',
+  to: '/about'
+}]
